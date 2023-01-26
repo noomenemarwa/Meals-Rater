3333b1274d40efdb814860acf4b2b1eb1d4cc967
Marwa Noomene <noomenemarwa26@gmail.com>
2023-01-26 15:12:44 +0100

    first commit

---
+++ penv/mealrater/api/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from.models import Meal, Rating
+from rest_framework import serializers, status
+from .models import Meal, Rating
+
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'password')
+        extra_kwargs = {'password': {'write_only': True, 'required': True}}
+
+
+# Meal serializer
+class MealSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Meal
+        fields = ('id', 'title', 'description', 'no_of_ratings', 'avg_rating')
+
+
+# Rating serializer
+class RatingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Rating
+        fields = ('id', 'stars', 'user', 'meal')
+       
